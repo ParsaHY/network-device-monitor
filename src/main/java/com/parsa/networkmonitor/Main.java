0f87953aa5ff4659850503b0b65c3f14abaa9c09
ParsaHY <95613708+ParsaHY@users.noreply.github.com>
2026-08-10 23:53:08 +0330

    Refactor DeviceManager getAllDevices test and add findDeviceById test

--- src/main/java/com/parsa/networkmonitor/Main.java
+++ src/main/java/com/parsa/networkmonitor/Main.java
@@ -10,15 +10,18 @@ public class Main {
         Device switch01 = new Device(2, "Switch-01", "192.168.1.2");
         Device server01 = new Device(3, "Server-01", "192.168.1.3");
 
-        DeviceManager deviceManager = new DeviceManager();
-        deviceManager.addDevice(router01);
-        deviceManager.addDevice(switch01);
-        deviceManager.addDevice(server01);
+        DeviceManager manager = new DeviceManager();
+        manager.addDevice(router01);
+        manager.addDevice(switch01);
+        manager.addDevice(server01);
 
-        List<Device> devices = deviceManager.getAllDevices();
+        List<Device> devices = manager.getAllDevices();
         for (int i = 0; i < devices.size(); i++) {
             System.out.println(devices.get(i));
         }
+
+        System.out.println("---------\n" + manager.findDeviceById(2));
+        System.out.println(manager.findDeviceById(99));
     }
 
 }
